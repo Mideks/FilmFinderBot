49dacbce9090fd8c1f918e0939a5575aca4a5b49
Mideks <MideksYT@gmail.com>
2024-04-22 02:22:11 +0500

    Add deleting /start command message

--- routers/commands.py
+++ routers/commands.py
@@ -9,9 +9,11 @@ from search_filters import SearchFilters
 router = Router()
 
 
+# todo: delete old bot message, if we know it
 @router.message(CommandStart())
 async def command_start_handler(message: Message, state: FSMContext) -> None:
     await state.clear()
     await state.update_data(search_filters=SearchFilters())
     await message.answer(f"Добро пожаловать! Я бот для поиска фильмов и сериалов!",
                          reply_markup=get_menu_keyboard())
+    await message.delete()
