642b0da06cbe3d49688363fbcaabfe25b4a739b0
Mideks <MideksYT@gmail.com>
2024-04-23 23:51:58 +0500

    Add text length tester

---
+++ test_utils/check_texts_length.py
@@ -0,0 +1,10 @@
+from tinydb import TinyDB
+
+from routers.film_card import generate_film_card_text
+
+db = TinyDB(r'..\films\info.json')
+
+for film in db.all():
+    text = generate_film_card_text(film)
+    if len(text) > 1024:
+        print(f"{film['title']}: текст слишком длинный ({len(text)}/1024)")
